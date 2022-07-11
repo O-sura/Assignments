35e78ece65315ef71563d923be18ccd3e246f8a5
O-sura <84634723+O-sura@users.noreply.github.com>
2022-07-11 07:06:39 +0530

    Update Q1.scala

--- Tutorial_2/Q1.scala
+++ Tutorial_2/Q1.scala
@@ -7,14 +7,14 @@ object Main{
         val g = 4.0f;
         val c = 'X';  
 
-        println(k + 12 * m);
-        println(m/j);
-        println(n%j);
-        println(m/j*j);
-        println(f+10*5+g);
+        println(k + 12 * m); //62
+        println(m/j); //2
+        println(n%j); //1
+        println(m/j*j); //4
+        println(f+10*5+g); //66.0
 
         //In scala the prefix and postfix incrementing features are not available
         //To obtain the same functionality following expression is been used
-        println((1+i)*n);
+        println((1+i)*n); //15
     }
 }
